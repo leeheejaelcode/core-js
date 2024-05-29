5f5d4b69746c1a2a46bb8d6087f21c7b6e7f9dc9
heejae <leeheejaelcode@gmail.com>
2024-05-29 17:03:16 +0900

    변수와 상수의 이해

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,42 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let name = 'john';
+admin = name;
+console.log(admin);
+
+const OURPLANET = 'earth';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calculateProductpriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductsPrice;
+
+// - 1년 기준 일(day)자 수
+const DAY_PER_YEAR = 365;
+
+// - 구매 결제 여부
+let isPayment = false;
+let hasCartList = false;
+// 변수명 앞에 is,has가 붙으면 관례적으로 boolean data가 들어오는 경우가 많다
+
+// - 구매 결제 내역
+let paymentlist;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Stussy';
+
+// - 오늘의 운세
+let fortuneToday = 'so good';
+
+// - 상품 정보
+const productInformation = '';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
