d5c0f3209489d7cc9450c02865a6b92418fc40d8
heejae <leeheejaelcode@gmail.com>
2024-05-30 16:24:08 +0900

    형변환의 이해

---
+++ client/chapter/core/07.typeConversion.js
@@ -0,0 +1,75 @@
+/* --------------------- */
+/* Type Conversion       */
+/* --------------------- */
+
+/* 데이터 → 문자 ----------------------------------------------------------- */
+
+// number
+const YEAR = 2024;
+
+console.log(String(YEAR));
+console.log(typeof (YEAR + ''));
+
+// undefined, null
+
+let days = null;
+
+console.log(days);
+console.log(days + ''); // 문자 null로 형변환
+
+let friends;
+console.log(friends);
+console.log(friends + ''); // 문자 undefined로 형변환
+
+// boolean
+
+let isClicked = true;
+
+console.log(String(isClicked));
+console.log('isClicked', isClicked);
+
+/* 데이터 → 숫자 ----------------------------------------------------------- */
+
+// undefined
+
+let friend;
+console.log(Number(friend));
+
+// null
+
+let money = null;
+console.log(Number(money));
+
+// boolean
+
+let isMarried = true;
+console.log(Number(isMarried));
+
+// string
+console.log('문자 -> 숫자');
+
+let num = '100';
+
+console.log(Number(num)); // 명시적 형변환
+console.log(num * 1); // 암시적 형변환환
+console.log(num / 1); // 암시적 형변환
+console.log(+num); // 암시적 형변환
+
+// numeric string
+const width = '120.5px';
+console.log('문자+숫자 -> 숫자');
+
+console.log(Number(width));
+console.log(parseInt(width, 10)); // 뒤에 숫자는 n진수
+
+/* 데이터 → 불리언 ---------------------------------------------------------- */
+// null, undefined, 0, NaN, ''
+// 위에 나열한 것 이외의 것들
+
+console.log('데이터 -> 불리언');
+console.log(Boolean(friend));
+console.log(Boolean(0));
+console.log(Boolean(''));
+console.log(Boolean(NaN));
+console.log(Boolean(' '));
+console.log(Boolean('0'));
