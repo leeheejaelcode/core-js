683c2d983ac759e5b5077fb4642598ab8d4c2666
heejae <leeheejaelcode@gmail.com>
2024-06-03 14:56:24 +0900

    옵셔널 체이닝 설명 추가

--- client/chapter/core/09-2.condition.js
+++ client/chapter/core/09-2.condition.js
@@ -29,21 +29,32 @@ console.log('whichFalsy : ', whichFalsy);
 let whichTruthy = false || '' || [2, 3].length || { thisIsTruthy: true };
 console.log('whichTruthy : ', whichTruthy);
 
-let userName = prompt('누구세요?');
+function login() {
+  let userName = prompt('누구세요?');
 
-if (userName?.toLowerCase() == 'admin') {
-  let passWord = prompt('비밀번호는요?');
+  // userName 이 null, undefined => 아래 코드 실행 안함.
 
-  if (passWord?.toLowerCase() == 'themaster') {
-    // 옵셔널 체이닝 활용..
-    console.log('환영합니다');
-  } else if (passWord == null) {
-    console.log('취소되었습니다.');
+  if (userName == null || undefined) return;
+
+  if (userName.toLowerCase() == 'admin') {
+    // 옵셔널 체이닝 null이나 undefined가 나오면 다음 조건이 실행되지않게
+
+    let passWord = prompt('비밀번호는요?');
+
+    if (passWord.toLowerCase() == 'themaster') {
+      // 옵셔널 체이닝 활용..
+      console.log('환영합니다');
+    } else if (passWord == null) {
+      console.log('취소되었습니다.');
+    } else {
+      console.log('인증에 실패하였습니다.');
+      login();
+    }
+  } else if (userName === null || userName?.replace(/\s*/g, '') === '') {
+    console.log('취소');
   } else {
-    console.log('인증에 실패하였습니다.');
+    console.log('실패');
   }
-} else if (userName === null || userName?.replace(/\s*/g, '') === '') {
-  console.log('취소');
-} else {
-  console.log('실패');
 }
+
+login();
