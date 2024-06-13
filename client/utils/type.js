03444209eee29ec6afc8f93f0618ade7478e48eb
heejae <leeheejaelcode@gmail.com>
2024-06-13 17:59:26 +0900

    type.js 추가

--- client/utils/type.js
+++ client/utils/type.js
@@ -1,5 +1,13 @@
-function isObject(data) {
-  return (
-    Object.prototype.toString.call(data).slice(8, -1).toLowerCase() === 'object'
-  );
-}
+const typeOf = (data) =>
+  Object.prototype.toString.call(data).slice(8, -1).toLowerCase();
+
+const isNumber = (data) => typeOf(data) === 'number';
+const isString = (data) => typeOf(data) === 'string';
+const isArray = (data) => typeOf(data) === 'array';
+const isObject = (data) => typeOf(data) === 'object';
+const isSymbol = (data) => typeOf(data) === 'symbol';
+const isNull = (data) => typeOf(data) === 'null';
+const isUndefined = (data) => typeOf(data) === 'undefined';
+const isBigInt = (data) => typeOf(data) === 'bigint';
+const isFunction = (data) => typeOf(data) === 'function';
+const isMath = (data) => typeOf(data) === 'math';
