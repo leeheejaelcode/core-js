b11584993b8980e266fafe5c484c7a0b6b37216a
heejae <leeheejaelcode@gmail.com>
2024-06-20 10:46:21 +0900

    starbucks 메뉴 이벤트
    - gsap version

--- client/starbucks/main.js
+++ client/starbucks/main.js
@@ -1,27 +1,18 @@
+/* -------------------------------------------------------------------------- */
+/*                                   gsap 버전                                  */
+/* -------------------------------------------------------------------------- */
+
 const aList = document.querySelectorAll('nav a');
-const depthList = document.querySelectorAll('.depth');
-const header = document.querySelector('#header');
 
-// 함수를 밖으로 빼서 사용하기
-const h = (t) => (t.style.height = '0');
-
-/* 두개의 코드는 같은 기능을 한다
- 전달하려는 아이템과 출력하는 아이템이 같을 때 사용이 가능하다.
- depthList.forEach((item)=>{
-  console.log(item)
- }) 
- depthList.forEach(console.log); */
+/* global gsap */
 
 aList.forEach((a) => {
-  a.addEventListener('mouseenter', () => {
-    const target = a.lastElementChild;
+  const target = a.lastElementChild;
 
-    // 모든 depth의 높이를 0으로
-    depthList.forEach(h);
+  const tl = gsap
+    .timeline({ paused: true })
+    .to(target, { height: 100, ease: 'power3.inOut' });
 
-    // target의 높이를 100px로
-    target.style.height = '100px';
-  });
+  a.addEventListener('mouseenter', () => tl.play());
+  a.addEventListener('mouseleave', () => tl.reverse());
 });
-
-header.addEventListener('mouseleave', () => depthList.forEach(h));
