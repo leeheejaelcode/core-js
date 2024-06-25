7f3e6dc8adb81dda68cc402571eb45b679768862
heejae <leeheejaelcode@gmail.com>
2024-06-25 09:10:58 +0900

    .

--- client/main.js
+++ client/main.js
@@ -1,108 +1,35 @@
+import data from './data/data.js';
+import { getNode, getRandom, insertLast, clearContents } from './lib/index.js';
 
+// [phase-1]
+// 1. 주접 떨기 버튼을 클릭 하는 함수
+//    - 주접 떨기 버튼 가져오기
+//    - 이벤트 연결하기 addEventListener('click')
 
+// 2. input 값 가져오기
+//    - input.value
 
+// 3. data함수에서 주접 1개 꺼내기
+//    - data(name)
+//    - getRandom()
 
-// named export (이름 내보내기)
+// 4. pick 항목 랜더링하기
 
-// default export (기본 내보내기) 무조건 1개 
+const submit = getNode('#submit');
+const nameField = getNode('#nameField');
+const result = getNode('.result');
 
-// rename, alias
+function handleSubmit(e) {
+  e.preventDefault();
 
-import { 
-  attr, 
-  getNode, 
-  insertLast,
-  clearContents, 
-} from "./lib/index.js";
+  const name = nameField.value;
+  const list = data(name);
+  const pick = list[getRandom(list.length)];
 
+  console.log(pick);
 
-
-
-function phase1(){
-  // 1. input value 값 가져오기 (first,second)
-  //    - input 선택하기 
-  //    - input에게 input 이벤트를 걸어준다.
-  //    - input.value 값을 가져온다.
-
-  // 2. 숫자 더하기
-  //    - 숫자 형변환 
-
-  // 3. result 내용 비우기
-  //    - clearContents
-
-  const first = getNode('#firstNumber');
-  const second = getNode('#secondNumber');
-  const result = getNode('.result');
-  const clear = getNode('#clear');
-
-
-  function handleInput(){
-    const firstValue = Number(first.value);
-    const secondValue = +second.value;
-    const total = firstValue + secondValue;
-
-    clearContents(result)
-    
-    insertLast(result,total);
-  }
-
-  function handleClear(e){
-    e.preventDefault();
-    
-    clearContents(first);
-    clearContents(second);
-    result.textContent = '-';
-  }
-
-  first.addEventListener('input',handleInput);
-  second.addEventListener('input',handleInput);
-  clear.addEventListener('click',handleClear);
-
+  clearContents(result);
+  insertLast(result, pick);
 }
 
-
-phase1()
-
-
-
-
-
-function phase2(){
-  const calculator = getNode('.calculator');
-  const result = getNode('.result');
-  const clear = getNode('#clear');
-  const numberInputs = [...document.querySelectorAll('input:not(#clear)')]
-  
-  
-  
-  function handleInput(){
-  
-    const total = numberInputs.reduce((acc,cur)=> acc + Number(cur.value),0)
-    
-    clearContents(result);
-    insertLast(result,total);
-  }
-  
-  
-  
-  
-  
-  function handleClear(e){
-    e.preventDefault();
-    numberInputs.forEach(clearContents);
-    result.textContent = '-';
-  }
-  
-  
-  
-  calculator.addEventListener('input',handleInput);
-  clear.addEventListener('click',handleClear);
-  
-}
-
-
-
-
-
-
-
+submit.addEventListener('click', handleSubmit);
