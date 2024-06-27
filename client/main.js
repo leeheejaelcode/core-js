53e81d520c5ff538a551fe4ce65ff66dce5ffe58
heejae <leeheejaelcode@gmail.com>
2024-06-27 11:41:27 +0900

    render시 로딩,에러 함수 사용

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+/* global gsap */
+
 // Repressentational State Transfer (REST API Architecture)
 
 // XHR
@@ -11,9 +13,17 @@
 // put body 내용만 나오게
 // patch body 내용만 변경되게
 
-import { tiger, getNode, renderUserCard } from './lib/index.js';
+import {
+  tiger,
+  getNode,
+  changeColor,
+  delayP,
+  renderSpinner,
+  renderUserCard,
+  renderEmptyCard,
+} from './lib/index.js';
 
-const ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
+const ENDPOINT = 'https://jsonplaceholder.typicode.com/user';
 
 // 1. user 데이터 fetch 해주세요.
 //     - tiger.get
@@ -25,10 +35,40 @@ const ENDPOINT = 'https://jsonplaceholder.typicode.com/users';
 const userCardInner = getNode('.user-card-inner');
 
 async function renderUserList() {
-  const response = await tiger.get(ENDPOINT);
-  const data = response.data;
+  // 에러처리할 때 promise-then / async await-then을 써도 되지만 try catch 가 가독성이 좋음
 
-  data.forEach((user) => renderUserCard(userCardInner, user));
+  // 데이터 통신에 걸리는 시간동안 svg 애니메이션 출력
+  renderSpinner(userCardInner);
+
+  await delayP(2000);
+
+  try {
+    // 유저의 정보를 가져오지 올때 처리
+    gsap.to('.loadingSpinner', {
+      opacity: 0,
+      onComplete() {
+        // 애니메이션이 끝난 다음에 자기 자신을 찾고 지우는
+        this._targets[0].remove();
+      },
+    });
+
+    const response = await tiger.get(ENDPOINT);
+    const data = response.data;
+
+    data.forEach((user) => renderUserCard(userCardInner, user));
+
+    changeColor('.user-card');
+
+    gsap.from('.user-card', {
+      y: -100,
+      opacity: 0,
+      stagger: 0.1,
+    });
+  } catch {
+    // 유저의 정보를 가져오지 못했을 때 처리
+    renderEmptyCard(userCardInner);
+    console.error('에러 발생');
+  }
 }
 
 renderUserList();
