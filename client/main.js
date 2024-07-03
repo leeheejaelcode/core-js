e23ddf0eeb983de6e0067528d11f46929029dc9a
heejae <leeheejaelcode@gmail.com>
2024-07-03 10:37:20 +0900

    웹 컴포넌트 -customized built-in elements

--- client/main.js
+++ client/main.js
@@ -1,37 +1,12 @@
-class Button extends HTMLElement {
+// 어떠한 html 엘리먼트를 사용할지
+class Button extends HTMLButtonElement {
   constructor() {
     super();
-    // console.log(this.id);
-    // console.log(this.label); label은 input의 표준이기때문에
-    // console.log(this.getAttribute('label')); 로 가져와야합니다
-    // console.log(this.dataset.label); 이렇게 가져옵니다
-    this.button = document.querySelector('button');
   }
-  connectedCallback() {
-    this._render();
-  }
+
+  connectedCallback() {}
+
   disconnectedCallback() {}
-  static get observedAttributes() {
-    return ['id'];
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (oldValue !== newValue) {
-      this._render();
-    }
-  }
-
-  _render() {
-    // 내가 수집한 button의 textContent를 나의 id값으로
-    this.button.textContent = this.id;
-  }
 }
-
-customElements.define('c-button', Button);
-
-const c = document.querySelector('c-button');
-
-let count = 0;
-c.addEventListener('click', () => {
-  c.setAttribute('id', ++count);
-});
+// extend에 정의된 버튼을 c-button으로 확장
+customElements.define('c-button', Button, { extends: 'button' });
