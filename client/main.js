13635be4f19f838b4825251ec424ec961456ab77
heejae <leeheejaelcode@gmail.com>
2024-07-03 11:14:45 +0900

    shadow Dom

--- client/main.js
+++ client/main.js
@@ -1,12 +1,28 @@
-// 어떠한 html 엘리먼트를 사용할지
-class Button extends HTMLButtonElement {
+class Button extends HTMLElement {
   constructor() {
     super();
+    // shadowDom을 열어두면 캡슐화가 되지 않은 상태
+    this.attachShadow({ mode: 'open' });
+    // shadowDom을 닫아놓으면 캡슐화가 된 상태
+    // this.attachShadow({ mode: 'closed' });
+
+    // this.attachShadow({ mode: 'closed' }); 닫힌 상태에서는
+    // 참조를 해야지 접근이 가능합니다.
+
+    // shadow돔은 캡슐화를 위해서 사용합니다.
+    this.shadowRoot.innerHTML = `
+      <button>hello</button>
+    `;
   }
 
   connectedCallback() {}
 
   disconnectedCallback() {}
 }
-// extend에 정의된 버튼을 c-button으로 확장
-customElements.define('c-button', Button, { extends: 'button' });
+
+customElements.define('c-button', Button);
+
+// elem.shadowRoot.querySelector('button')를 통해야지만 접근이 가능
+console.log(
+  document.querySelector('c-button').shadowRoot.querySelector('button')
+);
